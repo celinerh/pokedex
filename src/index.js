82fa41474336e6249a67346b5716f6ee8bfc928d
cellle <91531988+cellle@users.noreply.github.com>
2022-08-09 14:12:07 +0200

    setup router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Pokemon from "./pages/Pokemon";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/pokemon/:name" element={<Pokemon />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
