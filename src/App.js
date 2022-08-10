fbe088fa1ee3471f1bb0aa22fc9e1589d781c211
cellle <91531988+cellle@users.noreply.github.com>
2022-08-10 08:07:13 +0200

    setup app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import { useState } from "react";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+import { Outlet, Link } from "react-router-dom";
 
 function App() {
-  const [pokemons, setPokemons] = useState({});
+  const styles = {
+    navigation: css`
+      list-style: none;
+    `,
+    navigationLink: css`
+      text-decoration: none;
+    `,
+  };
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>Brians Fantastiske Pokemon-App</h1>
+        <nav css={styles.navigation}>
+          <li>
+            <Link to="/" css={styles.navigationLink}>
+              Forside
+            </Link>
+          </li>
+        </nav>
+      </header>
+
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
