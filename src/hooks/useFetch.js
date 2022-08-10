f9d7224b246ecfeaac03e2231bacbcbcafa0863c
cellle <91531988+cellle@users.noreply.github.com>
2022-08-10 08:06:51 +0200

    add fetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(function () {
+    (async function () {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setData(json);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+      }
+    })();
+  }, []);
+
+  return { data, isLoading, error };
+};
+
+export default useFetch;
